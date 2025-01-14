2551fb33d2a87725d00258524fa9640fa8ab6ea1
Chris Lundberg <chris@frakture.com>
2025-01-14 21:39:43 +0000

    Phone standardization

--- person_phone/transforms/inbound/extract_identifiers.js
+++ person_phone/transforms/inbound/extract_identifiers.js
@@ -5,17 +5,21 @@ module.exports = async function ({ batch }) {
   batch.forEach((e) => {
     e.identifiers = e.identifiers || [];
     if (e.phone) {
-      const hashable = e.phone.trim().toLowerCase();
+      // clean and add a plus
+      let phone = e.phone.replace(/[^0-9+]*/g, '').trim();
+      // clean us based phones.  If it's already prefixed with '+' then assume
+      // it's intentional
+      if (phone.length === 10 && phone.indexOf('+') < 0) phone = `+1${phone}`;
       // no secret or createHmac for this use case
       // it's basically a shared id hashing setup, so a secret will be exposed anyhow
       const value = createHash('sha256')
-        .update(hashable)
+        .update(phone)
         .digest('hex');
       e.identifiers.push({
         path: 'person_phone', type: 'phone_hash_v1', value,
       });
+      e.phone = phone;
       e.phone_hash_v1 = value;
-      e.foo = 'bar';
     }
   });
 

78c36436533a4bb1eb560b259737f324e5a571da
Chris Lundberg <chris@frakture.com>
2025-01-18 16:24:20 +0000

    Adapting interfaces

--- person_phone/transforms/inbound/upsert_tables.js
+++ person_phone/transforms/inbound/upsert_tables.js
@@ -1,20 +1,7 @@
-const fields = [
-  'person_id',
-  'remote_phone_id',
-  'remote_person_id',
-  'date_created',
-  'last_modified',
-  'do_not_call',
-  'phone',
-  'phone_hash_v1',
-  'source_input_id',
-  'primary',
-  'phone_type',
-  'preference_order',
-  'sms_status',
-  'sms_deliverability_score',
-  'call_status',
-];
+const schema = require('../../schema');
+
+const { columns } = schema.tables.find((d) => d.name === 'person_phone');
+
 module.exports = {
   bindings: {
     tablesToUpsert: { type: 'sql.tables.upsert' },
@@ -25,8 +12,19 @@ module.exports = {
       if (!o.phone) return;
       tablesToUpsert.person_phone = tablesToUpsert.person_phone || [];
       const vals = {};
-      fields.forEach((f) => {
-        if (o[f] !== undefined) vals[f] = o[f];
+      Object.keys(columns).forEach((name) => {
+        const def = columns[name];
+        if (o[name] !== undefined) { // if it's specified
+          if (o[name] === null) { // but null
+            if (def?.nullable === false) { // and not nullable
+              vals[name] = def?.default_value || '';
+            } else {
+              vals[name] = null;
+            }
+          } else {
+            vals[name] = o[name];
+          }
+        }
       });
       tablesToUpsert.person_phone.push(vals);
     });

7275056f11f5aafbdc173e6141bc25d909b98396
Chris Lundberg <chris@frakture.com>
2025-02-07 16:16:47 +0000

    Added new code to manage name updates and changes

---
+++ person/transforms/inbound/upsert_tables.js
@@ -0,0 +1,23 @@
+module.exports = {
+  bindings: {
+    tablesToUpsert: { type: 'sql.tables.upsert' },
+  },
+  async transform({
+    batch, tablesToUpsert,
+  }) {
+    if (batch.length === 0) return;
+
+    batch.forEach((o) => {
+      if (o.given_name || o.family_name) {
+        if (!o.person_id) throw new Error('Cannot update name, no person_id');
+
+        tablesToUpsert.person = (tablesToUpsert.person || []);
+        tablesToUpsert.person.push({
+          id: o.person_id,
+          given_name: o.given_name,
+          family_name: o.family_name,
+        });
+      }
+    });
+  },
+};

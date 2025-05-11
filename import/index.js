eae5d7d2c52788cba230b827069a969fac564155
Chris Lundberg <chris@frakture.com>
2025-05-11 18:17:17 +0000

    Add a new standard Import plugin

---
+++ import/index.js
@@ -0,0 +1,8 @@
+const metadata = {
+  name: '@engine9-io/engine9-interfaces/import',
+  version: '1.0.0',
+};
+
+module.exports = {
+  metadata,
+};
--- report/manifest.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: '@engine9-io/engine9-interfaces/report',
-  version: '1.0.0',
-  schemas: [
-  ],
-};

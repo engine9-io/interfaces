9d31caa7daf3570092eee8b87ae29eba0c70e154
Chris Lundberg <chris@frakture.com>
2025-07-17 01:15:19 +0000

    Lower case identifier work

--- person_remote/transforms/inbound/extract_identifiers.js
+++ person_remote/transforms/inbound/extract_identifiers.js
@@ -12,12 +12,12 @@ module.exports = {
         let value = null;
         if (parts.length > 1 && uuidIsValid(parts[0])) {
           // There may already be a pluginId prefixed with this
-          value = String(o.remote_person_id);
+          value = String(o.remote_person_id).toLowerCase();
         } else {
           // Otherwise prefix it with the current plugin
           if (!pluginId) throw new Error('A remote_person_id requires a pluginId as well, not found in options');
           if (!uuidIsValid(pluginId)) throw new Error("A pluginId was specified, but it's not a UUID. Perhaps you meant remote_plugin_id?");
-          value = `${pluginId}.${o.remote_person_id}`;
+          value = `${pluginId}.${o.remote_person_id}`.toLowerCase();// Must be case insensitive, because the unique indexes are as well
         }
         o.identifiers.push({
           path: 'person_remote', type: 'remote_person_id', value,

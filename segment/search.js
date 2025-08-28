b0b68a7f5e0fc7253b122f1a524133e7ec8d9a1f
Chris Lundberg <chris@frakture.com>
2025-08-28 19:43:59 +0000

    Segment search cleanup

--- segment/search.js
+++ segment/search.js
@@ -5,33 +5,51 @@ module.exports = {
         title: 'Segment',
         type: 'object',
         properties: {
-          segmentId: {
+          exclude: {
+            title: 'In/Not In',
+            description: '',
             type: 'string',
+            enum: ['Is in segment', 'Is not in segment']
           },
+          segmentId: {
+            title: 'Segment',
+            description: '',
+            type: 'string'
+          }
         },
-        required: [
-
-        ],
-      },
+        required: []
+      }
     },
     /* map from provided user data into an EQL structure */
     optionsToEQL(options) {
-      const { segmentId } = options;
+      const { exclude, segmentId } = options;
       const conditions = [];
+      let text = '';
       if (segmentId) {
+        text = ' segment ' + segmentId;
         conditions.push({
           type: 'EQUALS',
-          values: [{
-            ref: { column: 'segment_id' },
-          }, {
-            value: { value: segmentId },
-          }],
+          values: [
+            {
+              ref: { column: 'segment_id' }
+            },
+            {
+              value: { value: segmentId }
+            }
+          ]
         });
       }
-      return {
+      const eql = {
         table: 'person_segment',
-        conditions,
+        conditions
       };
-    },
-  },
+      if (exclude === 'Is not in segment') {
+        text = 'Is not in ';
+        o.exclude = true;
+      } else {
+        text = 'Is in ';
+      }
+      return { text, eql };
+    }
+  }
 };

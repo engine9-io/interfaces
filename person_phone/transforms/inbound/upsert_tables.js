63ebc13c5a9e5a0fbb575d9af2c324285ec29bc5
Chris Lundberg <chris@frakture.com>
2025-09-09 21:20:57 +0000

    Phone handling

--- person_phone/transforms/inbound/upsert_tables.js
+++ person_phone/transforms/inbound/upsert_tables.js
@@ -2,7 +2,7 @@ module.exports = {
   type: 'upsert',
   bindings: {
     tablesToUpsert: { path: 'sql.tables.upsert' },
-    databasePhones: { path: 'sql.query', options: { table: 'person_phone', lookup: ['phone'] } },
+    databasePhones: { path: 'sql.query', options: { table: 'person_phone', lookup: ['phone'] } }
   },
 
   async transform({ batch, tablesToUpsert, databasePhones }) {
@@ -14,27 +14,32 @@ module.exports = {
 
       const matchingPhones = databasePhones.filter((d) => d.phone === o.phone);
 
-      const personPhones = matchingPhones.filter(
-        (em) => o.person_id && (em.person_id === o.person_id),
-      );
+      const personPhones = matchingPhones.filter((em) => o.person_id && em.person_id === o.person_id);
       if (personPhones.length > 1) {
-        throw new Error(`Cannot update phone, there are 2 database entries for person_id ${o.person_id} with phone ${o.phone}`);
+        throw new Error(
+          `Cannot update phone, there are 2 database entries for person_id ${o.person_id} with phone ${o.phone}`
+        );
       }
+      const sms_status=o.sms_status || personPhones[0]?.sms_status || 'Subscribed';
 
       if (personPhones[0]) {
-        tablesToUpsert.person_phone.push({ ...personPhones[0], ...o });
+        tablesToUpsert.person_phone.push({
+          ...personPhones[0],
+          ...o,
+          sms_status
+        });
       } else {
         tablesToUpsert.person_phone.push({
           id: null,
           person_id: o.person_id,
           phone: o.phone,
-          sms_status: o.sms_status || 'Subscribed',
           ...o,
-          source_input_id: o.input_id,
+          source_input_id: o.input_id
+          sms_status
         });
       }
     });
 
     return batch;
-  },
+  }
 };

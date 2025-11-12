01aad9f62297d40222a969e95dd55101ef74d7e9
Chris Lundberg <chris@frakture.com>
2025-11-12 16:33:35 +0000

    Change to remote_entry_id from remote_transaction_id for standards

--- transaction/schema.js
+++ transaction/schema.js
@@ -9,21 +9,22 @@ module.exports = {
         entry_type_id: 'int',
         person_id: 'person_id',
         source_code_id: 'foreign_id',
-        remote_transaction_id: 'string',
+        remote_entry_id: 'string',
         amount: 'currency',
         refund_amount: 'currency',
         recurs_id: {
           type: 'int',
           nullable: false,
-          default_value: 0,
+          default_value: 0
         },
-        recurring_number: 'int',
+        recurring_number: 'int'
       },
       indexes: [
         { columns: 'id', primary: true },
         { columns: 'ts' },
         { columns: 'person_id' },
-      ],
-    },
-  ],
+        { columns: 'remote_entry_id' }
+      ]
+    }
+  ]
 };

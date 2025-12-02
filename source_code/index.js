e5398cdd138a7a92b05c8941051833cccf6871ea
Chris Lundberg <chris@frakture.com>
2025-12-02 02:16:27 +0000

    More metrics

--- source_code/index.js
+++ source_code/index.js
@@ -2,13 +2,15 @@ const metadata = {
   name: '@engine9-io/interfaces/source_code',
   version: '1.0.0',
   dependencies: {
-    '@engine9-io/interfaces/person': '>1.0.0',
-  },
+    '@engine9-io/interfaces/person': '>1.0.0'
+  }
 };
 
 const schema = require('./schema');
+const metrics = require('./metrics');
 
 module.exports = {
   metadata,
   schema,
+  metrics
 };

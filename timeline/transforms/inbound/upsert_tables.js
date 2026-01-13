236014e065ae1c09a081493aafaa5be294270993
Chris Lundberg <chris@frakture.com>
2026-01-13 01:14:40 +0000

    New interfaces

--- timeline/transforms/inbound/upsert_tables.js
+++ timeline/transforms/inbound/upsert_tables.js
@@ -1,23 +1,20 @@
 module.exports = {
   bindings: {
-    tablesToUpsert: { path: 'sql.tables.upsert' },
+    tablesToUpsert: { path: 'sql.tables.upsert' }
   },
-  async transform({
-    batch, tablesToUpsert, inputId,
-  }) {
-    tablesToUpsert.timeline = (tablesToUpsert.timeline || []);
+  async transform({ batch, tablesToUpsert, inputId }) {
+    tablesToUpsert.timeline = tablesToUpsert.timeline || [];
     batch.forEach((o) => {
       if (o.timeline_id) {
         tablesToUpsert.timeline.push({
           id: o.timeline_id,
-          ts: new Date(),
+          ts: o.ts || new Date(),
           input_id: inputId,
           entry_type_id: o.entry_type_id,
           person_id: o.person_id,
-          source_code_id: o.source_code_id,
+          source_code_id: o.source_code_id
         });
       }
     });
-  },
-
+  }
 };
